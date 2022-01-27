8130be14890e6aba28650d2344467fc91db21a5c
Andreina Suescum <andreinasuescum@gmail.com>
2022-01-27 10:26:06 -0500

    portfolio

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+const btnBars = document.querySelector('.bars');
+const SideNav = document.querySelector('.aside');
+
+
+btnBars.addEventListener('click', () => myFunc('open'));
+btnTimes.addEventListener('click', () => myFunc('close'));
+
+const myFunc = (navCondition) => {
+    if(navCondition === 'open'){
+        SideNav.classList.add('show-nav');
+        btnTimes.style.display = "block";
+        btnBars.style.display = "none";
+    }
+    else if(navCondition === 'close'){
+            SideNav.classList.remove('show-nav');
+            btnTimes.style.display = "none";
+            btnBars.style.display = "block";
+    }
+}
